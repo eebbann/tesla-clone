7d5b3c2fc434fcda0cc3772b379d71a128041da9
eebann <ebanemmanuel@live.com>
2021-09-15 11:37:14 -0500

    car-component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarItem from './components/CarItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <CarItem />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +19,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
